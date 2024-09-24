90af6b68300e8b80887efb76fed3b7adf5484b12
Екатерина Кушнер <kushnerxk0108@icloud.com>
2024-09-24 12:06:04 +0700

    Перенаправление адресов

--- app.py
+++ app.py
@@ -6,20 +6,20 @@ def not_found(err):
     return "нет такой страницы", 404
 
 @app.route("/")
-@app.route("/web")
+@app.route("/lab1/web")
 def web() :
     return """<!doctype html>
         <html>
            <body>
                <h1>web-сервер на flask</h1> 
-               <a href="/author">author</a>
+               <a href="/lab1/author">author</a>
            </body>
         </html>""", 200, {
             'X-Server': 'sample',
             'Content-Type': 'text/html; charset=utf-8'
         }
 
-@app.route("/author")
+@app.route("/lab1/author")
 def author() :
     name = "Кушнер Екатерина Константиновна"
     group = "ФБИ-21"
@@ -31,7 +31,7 @@ def author() :
                 <p>Студент: """ + name + """</p>
                 <p>Группа: """ + group + """</p>
                 <p>Факультет: """ + faculty + """</p>
-                <a href="/web">web</a>
+                <a href="/lab1/web">web</a>
             </body>
         </html>""" 
 
@@ -84,9 +84,9 @@ def reset_counter():
 </html>
 '''
 
-@app.route("/info")
+@app.route("/lab1/info")
 def info():
-    return redirect("/author")
+    return redirect("/lab1/author")
 
 @app.route("/lab1/created")
 def created():
