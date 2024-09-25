f52ce4936739d5f10491a17fb5ef037977d6806c
Екатерина Кушнер <kushnerxk0108@icloud.com>
2024-09-25 16:56:36 +0700

    Передача переменных в шаблон

--- app.py
+++ app.py
@@ -349,4 +349,8 @@ def add_flower(name):
 @app.route('/lab2/example')
 def example ():
     name = 'Кушнер Екатерина'
-    return render_template('example.html', name=name)
+    number = '2'
+    group = 'ФБИ-21'
+    course = '3'
+    return render_template('example.html', name=name, number=number, group=group, course=course)
+
