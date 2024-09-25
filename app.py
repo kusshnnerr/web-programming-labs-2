522a869a0ed3c156b18b2fe04fe750a071d268ba
Екатерина Кушнер <kushnerxk0108@icloud.com>
2024-09-25 16:49:05 +0700

    Имя студента

--- app.py
+++ app.py
@@ -348,4 +348,5 @@ def add_flower(name):
 
 @app.route('/lab2/example')
 def example ():
-    return render_template('example.html')
+    name = 'Кушнер Екатерина'
+    return render_template('example.html', name=name)
