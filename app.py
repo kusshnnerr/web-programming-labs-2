4a76e7e6c04fa4e6f561286d05e94930da0f28a0
Екатерина Кушнер <kushnerxk0108@icloud.com>
2024-10-06 21:28:53 +0700

    Цикл в шаблоне

--- app.py
+++ app.py
@@ -348,11 +348,14 @@ def add_flower(name):
 
 @app.route('/lab2/example')
 def example ():
-    name = 'Кушнер Екатерина'
-    number = '2'
-    group = 'ФБИ-21'
-    course = '3'
-    return render_template('example.html', name=name, number=number, group=group, course=course)
-
-
-
+    name, number, group, course = 'Кушнер Екатерина', 2, 'ФБИ-21', 3
+    fruits = [
+        {'name': 'яблоки', 'price': 100},
+        {'name': 'груши', 'price': 120},
+        {'name': 'апельсины', 'price': 80},
+        {'name': 'мандарины', 'price': 95},
+        {'name': 'манго', 'price': 321}
+    ]
+    return render_template('example.html', 
+                            name=name, number=number, group=group,
+                            course=course, fruits=fruits)
