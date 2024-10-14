da84707d0ab1d843b87352c29a080abc223e076a
Екатерина Кушнер <kushnerxk0108@icloud.com>
2024-10-14 18:08:18 +0700

    Загатовка к третьей лабораторной работе

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, url_for, redirect, render_template
 from lab1 import lab1 
 from lab2 import lab2
+from lab3 import lab3
 
 app = Flask(__name__)
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
+app.register_blueprint(lab3)
 
 @app.errorhandler(404)
 def not_found(err):
@@ -135,8 +137,9 @@ def index():
     <body>
         <header>НГТУ, ФБ, WEB-программирование, часть 2. Список лабораторных</header>
         <ol>
-            <li><a href="/lab1">Первая лабораторная</a></li>
-            <li><a href="/lab2/">Вторая лабораторная</a></li>
+            <li><a href="/lab1">Лабораторная работа №1</a></li>
+            <li><a href="/lab2/">Лабораторная работа №2</a></li>
+            <li><a href="/lab3/">Лабораторная работа №3</a></li>
         </ol>
     </body>
     <footer>
